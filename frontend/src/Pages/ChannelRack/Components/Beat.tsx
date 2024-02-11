c840cf2e9a7cf5109706f0736386bb9deaada198
siqqdev <okopniy.nikita@gmail.com>
2024-02-11 02:54:46 +0100

    fixed a minor color bug

--- frontend/src/Pages/ChannelRack/Components/Beat.tsx
+++ frontend/src/Pages/ChannelRack/Components/Beat.tsx
@@ -10,7 +10,7 @@ const Beat: React.FC<{ i: number; arrangement: ArrangementType[]; sound: string 
         <div
             className={`w-4 h-6 border-black border-2 rounded-md border-opacity-50 flex flex-col justify-end ${
                 i === 0 || i % 4 === 0 ? 'bg-yellow-300' : 'bg-neutral-500'
-            } ${hasSound ? 'bg-red-300' : ''} transition-all ${currentIndex === i && i % 4 || i === 0 ? 'bg-opacity-70' : currentIndex === i ? 'bg-opacity-40' : 'bg-opacity-100'}`}
+            } ${hasSound ? 'bg-red-400' : ''} transition-all ${currentIndex === i && i % 4 || i === 0 ? 'bg-opacity-70' : currentIndex === i ? 'bg-opacity-40' : 'bg-opacity-100'}`}
         >
             <div className="w-full"></div>
         </div>
