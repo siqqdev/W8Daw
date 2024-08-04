4801e2b7534da862908e3e113e6cffd7f465b105
siqq <144137886+siqqdev@users.noreply.github.com>
2024-08-04 17:30:46 +0200

    Update Project.tsx

--- frontend/src/Pages/Project.tsx
+++ frontend/src/Pages/Project.tsx
@@ -2,6 +2,7 @@ import ChannelRack from './ChannelRack/ChannelRack'
 import ProjectSettings from './ChannelRack/Components/ProjectSettings/ProjectSettings'
 
 const Project = () => {
+// @ts-ignore
   console.log(window.Telegram.WebApp.initData)
   return (
         <div className='h-screen flex justify-center bg-neutral-900 overflow-hidden flex-col gap-6'>
